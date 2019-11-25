718770e1d30d12711873ec45a121b08d2f658b8e
Thiago Bôa <thiagoboat@gmail.com>
2019-11-24 22:13:44 -0300

    Adds styled components to button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,32 @@
 import React from "react";
-import { defaultCipherList } from "constants";
+import styled, { css, ThemeProvider } from "styled-components";
+import theme from "styled-theming";
+
+const white = "#fff";
+const black = "#000";
+
+const buttonStyles = theme("mode", {
+  light: css`
+    background: ${white};
+    color: ${black};
+  `,
+  dark: css`
+    background: ${black};
+    color: ${white};
+  `
+});
+
+const StyledButton = styled.button`
+  ${buttonStyles}
+  border-radius: 3px;
+`;
 
 function Button(props) {
-  return <button>{props.children}</button>;
+  return (
+    <ThemeProvider theme={{ mode: "dark" }}>
+      <StyledButton>{props.children}</StyledButton>
+    </ThemeProvider>
+  );
 }
 
 export default Button;
